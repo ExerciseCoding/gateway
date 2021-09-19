a4aeb52ddd2a08e19ce07d92bb384770c969ed9f
SunshineDay <924345620@qq.com>
2021-09-19 08:50:27 +0800

    update main

--- main.go
+++ main.go
@@ -12,7 +12,6 @@ func main()  {
 	lib.InitModule("./conf/dev/",[]string{"base","mysql","redis",})
 	defer lib.Destroy()
 	router.HttpServerRun()
-
 	quit := make(chan os.Signal)
 	signal.Notify(quit, syscall.SIGKILL, syscall.SIGQUIT, syscall.SIGINT, syscall.SIGTERM)
 	<-quit
