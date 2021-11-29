5eb2cf941d3ede2d164f8d2df7bff127fe1a62d7
SunshineDay <924345620@qq.com>
2021-11-29 23:19:21 +0800

    应用管理

--- controller/app.go
+++ controller/app.go
@@ -1,1 +1,144 @@
 package controller
+
+import (
+	"errors"
+	"gateway/dao"
+	"gateway/dto"
+	"gateway/middleware"
+	"github.com/e421083458/gin_scaffold/public"
+	"github.com/e421083458/golang_common/lib"
+	"github.com/gin-gonic/gin"
+)
+
+type  APPController struct {}
+
+func AppRegister(group *gin.RouterGroup){
+	app := &APPController{}
+	group.GET("/app_list", app.APPList)
+
+}
+
+func (admin *APPController) APPList(c *gin.Context){
+	params := &dto.APPListInput{}
+	if err := params.GetVaildParams(c); err != nil{
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	info := &dao.App{}
+	list, total, err := info.AppList(c, lib.GORMDefaultPool,params)
+	if err != nil{
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+
+	outputList := []dto.APPListItemOutput{}
+	for _, item := range list{
+
+	}
+}
+
+// APPDetail godoc
+// @Summary 租户详情
+// @Description 租户详情
+// @Tags 租户管理
+// @ID /app/app_detail
+// @Accept  json
+// @Produce  json
+// @Param id query string true "租户ID"
+// @Success 200 {object} middleware.Response{data=dao.App} "success"
+// @Router /app/app_detail [get]
+func (admin *APPController) APPDetail(c *gin.Context) {
+	params := &dto.APPDetailInput{}
+	if err := params.GetValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	search := &dao.App{
+		ID: params.ID,
+	}
+	detail, err := search.Find(c, lib.GORMDefaultPool, search)
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	middleware.ResponseSuccess(c, detail)
+	return
+}
+
+// APPDelete godoc
+// @Summary 租户删除
+// @Description 租户删除
+// @Tags 租户管理
+// @ID /app/app_delete
+// @Accept  json
+// @Produce  json
+// @Param id query string true "租户ID"
+// @Success 200 {object} middleware.Response{data=string} "success"
+// @Router /app/app_delete [get]
+func (admin *APPController) APPDelete(c *gin.Context) {
+	params := &dto.APPDetailInput{}
+	if err := params.GetValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+	search := &dao.App{
+		ID: params.ID,
+	}
+	info, err := search.Find(c, lib.GORMDefaultPool, search)
+	if err != nil {
+		middleware.ResponseError(c, 2002, err)
+		return
+	}
+	info.IsDelete = 1
+	if err := info.Save(c, lib.GORMDefaultPool); err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
+	return
+}
+
+// AppAdd godoc
+// @Summary 租户添加
+// @Description 租户添加
+// @Tags 租户管理
+// @ID /app/app_add
+// @Accept  json
+// @Produce  json
+// @Param body body dto.APPAddHttpInput true "body"
+// @Success 200 {object} middleware.Response{data=string} "success"
+// @Router /app/app_add [post]
+func (admin *APPController) AppAdd(c *gin.Context) {
+	params := &dto.APPAddHttpInput{}
+	if err := params.GetValidParams(c); err != nil {
+		middleware.ResponseError(c, 2001, err)
+		return
+	}
+
+	//验证app_id是否被占用
+	search := &dao.App{
+		AppID: params.AppID,
+	}
+	if _, err := search.Find(c, lib.GORMDefaultPool, search); err == nil {
+		middleware.ResponseError(c, 2002, errors.New("租户ID被占用，请重新输入"))
+		return
+	}
+	if params.Secret == "" {
+		params.Secret = public.MD5(params.AppID)
+	}
+	tx := lib.GORMDefaultPool
+	info := &dao.App{
+		AppID:    params.AppID,
+		Name:     params.Name,
+		Secret:   params.Secret,
+		WhiteIPS: params.WhiteIPS,
+		Qps:      params.Qps,
+		Qpd:      params.Qpd,
+	}
+	if err := info.Save(c, tx); err != nil {
+		middleware.ResponseError(c, 2003, err)
+		return
+	}
+	middleware.ResponseSuccess(c, "")
+	return
+}
